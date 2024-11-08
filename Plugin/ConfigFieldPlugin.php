d90326f966d932cb9ea141c3296dc60e56c5f938
AlexandeBulete <bulete.alexandre@gmail.com>
2024-11-09 00:59:24 +0100

    feat: Plugin trigger adminhtml

---
+++ Plugin/ConfigFieldPlugin.php
@@ -0,0 +1,200 @@
+<?php
+namespace Opengento\BetterBo\Plugin;
+
+use Magento\Config\Model\Config\Structure\Element\Field as Subject;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Escaper;
+use Magento\Framework\Phrase;
+use Magento\Store\Api\WebsiteRepositoryInterface;
+use Magento\Store\Api\StoreRepositoryInterface;
+use Magento\Store\Api\Data\WebsiteInterface;
+use Magento\Store\Api\Data\StoreInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class ConfigFieldPlugin
+{
+    const SCOPE_TYPE_WEBSITES = 'websites';
+    const SCOPE_TYPE_STORES = 'stores';
+
+    /** @var Escaper */
+    private $escaper;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+    /**
+     * @var WebsiteRepositoryInterface
+     */
+    private $websiteRepository;
+    /**
+     * @var StoreRepositoryInterface
+     */
+    private $storeRepository;
+    /**
+     * @var RequestInterface
+     */
+    private $request;
+
+    public function __construct(
+        Escaper $escaper,
+        ScopeConfigInterface $scopeConfig,
+        WebsiteRepositoryInterface $websiteRepository,
+        StoreRepositoryInterface $storeRepository,
+        RequestInterface $request
+    ) {
+        $this->escaper = $escaper;
+        $this->scopeConfig = $scopeConfig;
+        $this->websiteRepository = $websiteRepository;
+        $this->storeRepository = $storeRepository;
+        $this->request = $request;
+    }
+
+    /**
+     * @param Subject $subject
+     * @param string $result
+     * @return string
+     */
+    public function afterGetTooltip(Subject $subject, $result)
+    {
+        $lines = [$result];
+        foreach($this->websiteRepository->getList() as $website) {
+            if ($this->getWebsiteParam() || $this->getStoreParam()) {
+                continue;
+            }
+            // Only show website specific values in default scope
+            if ($scopeLine = $this->getScopeHint($subject, self::SCOPE_TYPE_WEBSITES, $website)) {
+                $lines[] = $scopeLine;
+            }
+        }
+        foreach($this->storeRepository->getList() as $store) {
+            if ($this->getStoreParam($store)) {
+                continue;
+            }
+            if (($websiteId = $this->getWebsiteParam()) && ($store->getWebsiteId() != $websiteId)) {
+                continue;
+            }
+            // Only show store specific values in default scope and in parent website scope
+            if ($scopeLine = $this->getScopeHint($subject, self::SCOPE_TYPE_STORES, $store)) {
+                $lines[] = $scopeLine;
+            }
+        }
+
+        return implode('<br />', array_filter($lines));
+    }
+
+    /**
+     * @param Subject $subject
+     * @param string $result
+     * @return string
+     */
+    public function afterGetComment(Subject $subject, $result)
+    {
+
+        if ($result instanceof Phrase) {
+            $result = (string) $result;
+        }
+
+        if (strlen(trim($result))) {
+            $result .= '<br />';
+        }
+
+        $result .= __('Path: <code>%1</code>', $this->getPath($subject));
+
+        return $result;
+    }
+
+    /**
+     * @param Subject $subject
+     * @return string
+     */
+    private function getPath(Subject $subject)
+    {
+        return $path = $subject->getConfigPath() ?: $subject->getPath();
+    }
+
+    /**
+     * @param Subject $field
+     * @param string $scopeType
+     * @param WebsiteInterface|StoreInterface $scope
+     * @return string
+     */
+    private function getScopeHint(Subject $field, $scopeType, $scope)
+    {
+        $path = $this->getPath($field);
+        $scopeLine = '';
+        if ($websiteId = $this->getWebsiteParam()) {
+            $currentValue = $this->scopeConfig->getValue(
+                $path,
+                ScopeInterface::SCOPE_WEBSITE,
+                $websiteId
+            );
+        } else {
+            $currentValue = $this->scopeConfig->getValue($path);
+        }
+        $scopeValue = $this->scopeConfig->getValue($path, $scopeType, $scope->getId());
+        
+        if (is_array($currentValue) || is_array($scopeValue)) {
+            return $scopeLine;
+        }
+        
+        $currentValue = (string) $currentValue;
+        $scopeValue = (string) $scopeValue;
+        
+        // dd($currentValue, $scopeValue);
+        if ($scopeValue != $currentValue) {
+            $scopeValue = $this->escaper->escapeHtml($scopeValue);
+
+            switch($scopeType) {
+                case self::SCOPE_TYPE_STORES:
+                    return __(
+                        'Store <code>%1</code>: "%2"',
+                        $scope->getCode(),
+                        $this->getValueLabel($field, $scopeValue)
+                    );
+                case self::SCOPE_TYPE_WEBSITES:
+                    return __(
+                        'Website <code>%1</code>: "%2"',
+                        $scope->getCode(),
+                        $this->getValueLabel($field, $scopeValue)
+                    );
+            }
+        }
+        return $scopeLine;
+    }
+
+    private function getValueLabel(Subject $field, string $scopeValue): string
+    {
+        $scopeValue = trim($scopeValue);
+        if ($field->hasOptions()) {
+            if ($field->getType() === 'multiselect' && strpos($scopeValue, ',') !== false) {
+                return implode(
+                    ', ',
+                    array_map(
+                        function ($key) use ($field) {
+                            return $this->getValueLabel($field, $key);
+                        },
+                        explode(',', $scopeValue)
+                    )
+                );
+            }
+            foreach ($field->getOptions() as $option) {
+                if (is_array($option) && $option['value'] == $scopeValue) {
+                    return $option['label'];
+                }
+            }
+        }
+        return $scopeValue;
+    }
+
+    private function getWebsiteParam(): ?string
+    {
+        return $this->request->getParam('website');
+    }
+
+    private function getStoreParam(): ?string
+    {
+        return $this->request->getParam('store');
+    }
+}
---
+++ Plugin/ProductEavDataProviderPlugin.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Opengento\BetterBo\Plugin;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\Eav;
+use Magento\Framework\Registry;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Catalog\Model\Attribute\ScopeOverriddenValue;
+
+class ProductEavDataProviderPlugin
+{
+    /** @var StoreManagerInterface */
+    private $storeManager;
+
+    /** @var Registry */
+    private $registry;
+
+    /** @var ProductRepositoryInterface */
+    private $productRepository;
+
+    /** @var array */
+    private $stores;
+
+    /**
+     * ProductEavDataProviderPlugin constructor.
+     *
+     * @param StoreManagerInterface $storeManager
+     * @param Registry $registry
+     * @param ProductRepositoryInterface $productRepository
+     */
+    public function __construct(
+        StoreManagerInterface $storeManager,
+        Registry $registry,
+        ProductRepositoryInterface $productRepository
+    ) {
+        $this->storeManager = $storeManager;
+        $this->registry = $registry;
+        $this->productRepository = $productRepository;
+    }
+
+    /**
+     * @param Eav $subject
+     * @param $result
+     * @return mixed
+     */
+    public function afterSetupAttributeMeta(Eav $subject, $result)
+    {
+        if (!isset($result['arguments']['data']['config']['code'])
+            || $result['arguments']['data']['config']['globalScope']
+            || $result['arguments']['data']['config']['code'] == 'quantity_and_stock_status'
+        ) {
+            return $result;
+        }
+
+        $scopeHints = [];
+        $attributeCode = $result['arguments']['data']['config']['code'];
+        $storeViews = $this->getStores();
+        $product = $this->registry->registry('current_product');
+
+        if ($product === null || $product->getId() === null) {
+            return $result;
+        }
+
+        foreach ($storeViews as $storeView) {
+            $productByStoreCode = $this->getProductInStoreView($product->getId(), $storeView->getId());
+            $currentScopeValueForCode = $value = $productByStoreCode->getData($attributeCode);
+
+            if ($result['arguments']['data']['config']['dataType'] == 'select'
+                && !is_array($currentScopeValueForCode)
+            ) {
+                $value = $productByStoreCode->getResource()->getAttribute($attributeCode)->getSource()->getOptionText($currentScopeValueForCode);
+            }
+
+            // This checks if we can cast $value to a string
+            // If this fails, we json_encode the value, so we eventually do get a string representation
+            // If the json_encode fails for some reason, we just ignore it and won't output anything
+            //
+            // This problem can be seen in practice when:
+            // - having at least 2 websites
+            // - having the catalog price scope set to Website
+            // - using different tier prices for a single product over multiple websites
+            // (Another bug in that particular case, is that the tooltip doesn't seem to show up, so that's another thing which will need to get fixed someday)
+            $valueAsString = null;
+            try {
+                $valueAsString = (string) $value;
+            } catch (\Throwable $ex) {
+                $valueAsString = json_encode($value);
+                if ($valueAsString === false) {
+                    $valueAsString = null;
+                }
+            }
+
+            if ($valueAsString !== null && $product->getData($attributeCode) !== $currentScopeValueForCode) {
+                $scopeHints[] = $storeView->getName() . ': ' . $valueAsString;
+            }
+
+            if (!empty($scopeHints)) {
+                $result['arguments']['data']['config']['tooltip']['description'] = implode('<br>', $scopeHints);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param int $productId
+     * @param int $storeViewId
+     * @return mixed
+     */
+    private function getProductInStoreView($productId, $storeViewId)
+    {
+        return $this->productRepository->getById($productId, false, $storeViewId);
+    }
+
+    /**
+     * @return array
+     */
+    private function getStores()
+    {
+        if (!$this->stores) {
+            $this->stores = $this->storeManager->getStores();
+        }
+
+        return $this->stores;
+    }
+}
